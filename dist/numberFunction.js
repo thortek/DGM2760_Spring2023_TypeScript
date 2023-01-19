8ac1eb5b8f071f4cb3b62a4d4a3bc0c4490107c6
Thor Anderson <ta.anderson@gmail.com>
2023-01-19 11:15:15 -0700

    numberFunction example

--- dist/numberFunction.js
+++ dist/numberFunction.js
@@ -1,3 +1,8 @@
 "use strict";
-console.log('Welcome to the TypeScript!');
-let foo = 'my typescript string';
+function add(num1, num2) {
+    return num1 + num2;
+}
+const numberOne = 25;
+const numberTwo = 3.5;
+let result = add(numberOne, numberTwo);
+console.log(result);
--- src/numberFunction.ts
+++ src/numberFunction.ts
@@ -1,6 +1,12 @@
-console.log('Welcome to the TypeScript!')
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
 
-let foo = 'my typescript string'
+const numberOne = 25
+const numberTwo = 3.5
+
+let result = add(numberOne, numberTwo)
+console.log(result)
 
 
 
