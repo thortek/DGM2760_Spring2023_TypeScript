e697ba9d465377b72eb9e30091c07b7c84560bc0
Thor Anderson <ta.anderson@gmail.com>
2023-04-04 10:59:34 -0600

    Fixed double click issue on next and prev

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -12,79 +12,176 @@ let gridPosition: number = 0
 
 let currentBlock: string
 
-const videoIFrame = document.querySelector('#challengeVid')! as HTMLIFrameElement
+const videoIFrame = document.querySelector(
+  '#challengeVid',
+)! as HTMLIFrameElement
 const prevButton = document.querySelector('#prev')! as HTMLButtonElement
 const challengeVidBtn = document.querySelector('#challengeVidBtn')!
 const nextButton = document.querySelector('#next')! as HTMLButtonElement
 const topMenu = document.querySelector('.menu')!
 
 interface VideoDisplayer {
-    videoUrl: string
-    display(theFrame: HTMLIFrameElement): void
+  videoUrl: string
+  display(theFrame: HTMLIFrameElement): void
 }
 
 class CodeSnippet implements VideoDisplayer {
-    constructor(public title: string, public description: string, public instructions: string, public videoUrl: string) {
-    }
-    display(theFrame: HTMLIFrameElement): void {
-        theFrame.src = this.videoUrl
-    }
+  constructor(
+    public title: string,
+    public description: string,
+    public instructions: string,
+    public videoUrl: string,
+  ) {}
+  display(theFrame: HTMLIFrameElement): void {
+    theFrame.src = this.videoUrl
+  }
 }
 
-const basicCssSnippets = basicCss.challenges.map((challenge: any) => new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl))
+const basicCssSnippets = basicCss.challenges.map(
+  (challenge: any) =>
+    new CodeSnippet(
+      challenge.title,
+      challenge.description,
+      challenge.instructions,
+      challenge.videoUrl,
+    ),
+)
 
-const basicHtmlSnippets = basicHTML.challenges.map((challenge: any) => new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl))
+const basicHtmlSnippets = basicHTML.challenges.map(
+  (challenge: any) =>
+    new CodeSnippet(
+      challenge.title,
+      challenge.description,
+      challenge.instructions,
+      challenge.videoUrl,
+    ),
+)
 
-const flexboxSnippets = flexbox.challenges.map((challenge: any) => new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl))
+const flexboxSnippets = flexbox.challenges.map(
+  (challenge: any) =>
+    new CodeSnippet(
+      challenge.title,
+      challenge.description,
+      challenge.instructions,
+      challenge.videoUrl,
+    ),
+)
 
-const gridSnippets = grid.challenges.map((challenge: any) => new CodeSnippet(challenge.title, challenge.description, challenge.instructions, challenge.videoUrl))
-
-console.log(basicCssSnippets.length, basicHtmlSnippets.length, flexboxSnippets.length, gridSnippets.length)
+const gridSnippets = grid.challenges.map(
+  (challenge: any) =>
+    new CodeSnippet(
+      challenge.title,
+      challenge.description,
+      challenge.instructions,
+      challenge.videoUrl,
+    ),
+)
 
 const populateMenu = (arrayOfBlocks: any[]) => {
-    arrayOfBlocks.forEach(block => {
-        const menuItem = document.createElement('li')
-        const menuLink = document.createElement('a')
-        menuLink.textContent = block?.meta?.name
+  arrayOfBlocks.forEach((block) => {
+    const menuItem = document.createElement('li')
+    const menuLink = document.createElement('a')
+    menuLink.textContent = block?.meta?.name
 
-        menuLink.addEventListener('click', () => {
-            const allMenuLinks = document.querySelectorAll('.menu li a')
-            allMenuLinks.forEach(link => {
-                link.classList.remove('active')
-            })
-            menuLink.classList.add('active')
-            currentBlock = block?.meta?.name
-        })
-
-        menuItem.appendChild(menuLink)
-        topMenu.appendChild(menuItem)
+    menuLink.addEventListener('click', () => {
+      const allMenuLinks = document.querySelectorAll('.menu li a')
+      allMenuLinks.forEach((link) => {
+        link.classList.remove('active')
+      })
+      menuLink.classList.add('active')
+      currentBlock = block?.meta?.name
+      const snippet = getCurrentSnippet(currentBlock)
+      if (snippet) {
+        snippet.display(videoIFrame)
+        challengeVidBtn.textContent = snippet.title
+      }
     })
+
+    menuItem.appendChild(menuLink)
+    topMenu.appendChild(menuItem)
+  })
 }
 
 populateMenu(blocksArray)
 
+const getCurrentSnippet = (block: string) => {
+  if (block === 'Basic CSS') {
+    return basicCssSnippets[cssPosition]
+  }
+  if (block === 'Basic HTML and HTML5') {
+    return basicHtmlSnippets[htmlPosition]
+  }
+  if (block === 'CSS Flexbox') {
+    return flexboxSnippets[flexboxPosition]
+  }
+  if (block === 'CSS Grid') {
+    return gridSnippets[gridPosition]
+  }
+}
+
+const updatePosition = (direction: string) => {
+  if (currentBlock === 'Basic CSS') {
+    if (direction === 'next') {
+      if (cssPosition < basicCssSnippets.length - 1) {
+        cssPosition++
+      }
+    } else {
+      if (cssPosition > 0) {
+        cssPosition--
+      }
+    }
+  }
+  if (currentBlock === 'Basic HTML and HTML5') {
+    if (direction === 'next') {
+      if (htmlPosition < basicHtmlSnippets.length - 1) {
+        htmlPosition++
+      }
+    } else {
+      if (htmlPosition > 0) {
+        htmlPosition--
+      }
+    }
+  }
+  if (currentBlock === 'CSS Flexbox') {
+    if (direction === 'next') {
+      if (flexboxPosition < flexboxSnippets.length - 1) {
+        flexboxPosition++
+      }
+    } else {
+      if (flexboxPosition > 0) {
+        flexboxPosition--
+      }
+    }
+  }
+  if (currentBlock === 'CSS Grid') {
+    if (direction === 'next') {
+      if (gridPosition < gridSnippets.length - 1) {
+        gridPosition++
+      }
+    } else {
+      if (gridPosition > 0) {
+        gridPosition--
+      }
+    }
+  }
+}
+
 nextButton.addEventListener('click', () => {
-    let snippet: CodeSnippet = new CodeSnippet('', '', '', '')
-    if (currentBlock === 'Basic CSS') {
-        snippet = basicCssSnippets[cssPosition]
-        cssPosition <= basicCssSnippets.length ? cssPosition++ : cssPosition = 0
-    }
-    if (currentBlock === 'Basic HTML and HTML5') {
-        snippet = basicHtmlSnippets[htmlPosition]
-        htmlPosition <= basicHtmlSnippets.length ? htmlPosition++ : htmlPosition = 0
-    }
-    if (currentBlock === 'CSS Flexbox') {
-        snippet = flexboxSnippets[flexboxPosition]
-        flexboxPosition <= flexboxSnippets.length ? flexboxPosition++ : flexboxPosition = 0
-    }
-    if (currentBlock === 'CSS Grid') {
-        snippet = gridSnippets[gridPosition]
-        gridPosition <= gridSnippets.length ? gridPosition++ : gridPosition = 0
-    }
-    if (snippet.videoUrl && snippet.title) {
-        snippet.display(videoIFrame)
-        challengeVidBtn.textContent = snippet.title
-    }
+  updatePosition('next')
+  const snippet = getCurrentSnippet(currentBlock)
+  if (snippet) {
+    snippet.display(videoIFrame)
+    challengeVidBtn.textContent = snippet.title
+  }
+})
+
+prevButton.addEventListener('click', () => {
+  updatePosition('prev')
+  const snippet = getCurrentSnippet(currentBlock)
+  if (snippet) {
+    snippet.display(videoIFrame)
+    challengeVidBtn.textContent = snippet.title
+  }
 })
 
 /* const buildSnippetDisplay = (challenge: any) => {
